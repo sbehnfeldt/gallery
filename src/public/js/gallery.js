cda738cc17a952f8908eaa1cece4d1c7cc334972
Stephen <stephen@behnfeldt.pro>
2024-03-25 07:51:46 -0400

    Add a 3rd row to gallery.  Alternate pushing new images on from right or left.

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -1,7 +1,7 @@
 (function (global, $) {
     'use strict';
     console.log("Document ready");
-    const INTERVAL = 5000
+    const INTERVAL = 3000
 
     let $rows = $('#gallery div.row');
     $rows.each((idx, row) => {
@@ -9,14 +9,29 @@
         setTimeout(() => {
             setInterval(() => {
                 let $img = $('<img>').attr('src', '/image.php?' + Date.now());
-                // $row.empty();
-                $row.append($img);
 
-                $img.on('load', () => {
-                    $row.find('img').filter(function () {
-                        return $(this).offset().left < $row.offset().left;
-                    }).remove()
-                });
+                if (idx % 2) {
+                    // Odd rows, push on from the right
+                    $row.append($img);
+                    $img.on('load', () => {
+                        let $imgs = $row.find('img').filter(function () {
+                            return $(this).offset().left < $row.offset().left;
+                        })
+                        $imgs.remove()
+                    });
+
+                } else {
+                    // Even rows, push on from the left
+                    $row.prepend($img);
+                    $img.on('load', () => {
+                        let $imgs = $row.find('img').filter(function () {
+                            return ($(this).offset().left + $(this).width()) > ($row.offset().left + $row.width());
+                        });
+
+                        $imgs.remove()
+                    });
+
+                }
             }, INTERVAL)
         }, idx * (INTERVAL / $rows.length))
     });

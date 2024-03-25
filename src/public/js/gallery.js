ee1eddd6b4a7b18ac0699cdd3fd9fab55ff60802
Stephen <stephen@behnfeldt.pro>
2024-03-25 05:53:06 -0400

    Remove an image when its left edge is pushed beyond the left edge of the containing row

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -8,6 +8,12 @@
         let $img = $('<img>').attr('src', '/image.php?' + Date.now());
         // $row.empty();
         $row.append($img);
+        $img.on('load', () => {
+            let $first = $row.find('img').eq(0);
+            if ($first.offset().left < $row.offset().left ) {
+                $first.remove()
+            }
+        });
     }, 3000)
 
 

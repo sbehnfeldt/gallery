37eb60697b2aa9400422c2c570952cb1b4edc998
Stephen <stephen@behnfeldt.pro>
2024-03-26 08:47:34 -0400

    Refactor gallery JS: out-line callbacks to simplify.

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -3,38 +3,43 @@
     console.log("Document ready");
     const INTERVAL = 6000
 
+
+    // Add image to end of a row (on the right)
+    function pushImage($row, $img) {
+        $row.append($img);
+        $img.on('load', () => {
+            $row.find('img').filter(function () {
+                return $(this).offset().left < $row.offset().left;
+            }).remove()
+        });
+    }
+
+    // Add image to start of a row (on the left)
+    function unshiftImage($row, $img) {
+        $row.prepend($img);
+        $img.on('load', () => {
+            $row.find('img').filter(function () {
+                return ($(this).offset().left + $(this).width()) > ($row.offset().left + $row.width());
+            }).remove();
+        });
+    }
+
+    function placeImg(idx, $row) {
+        let $img = $(`<img>`).attr('src', '/image.php?' + Date.now());
+        if (idx % 2) {
+            pushImage($row, $img);
+        } else {
+            unshiftImage($row, $img);
+        }
+    }
+
+
     let $rows = $('#gallery div.row');
     $rows.each((idx, row) => {
         let $row = $(row);
         setTimeout(() => {
-            setInterval(() => {
-                let $img = $(`<img>`).attr('src', '/image.php?' + Date.now());
-
-                if (idx % 2) {
-                    // Odd rows, push on from the right
-                    $row.append($img);
-                    $img.on('load', () => {
-                        let $imgs = $row.find('img').filter(function () {
-                            return $(this).offset().left < $row.offset().left;
-                        })
-                        $imgs.remove()
-                    });
-
-                } else {
-                    // Even rows, push on from the left
-                    $row.prepend($img);
-                    $img.on('load', () => {
-                        let $imgs = $row.find('img').filter(function () {
-                            return ($(this).offset().left + $(this).width()) > ($row.offset().left + $row.width());
-                        });
-
-                        $imgs.remove()
-                    });
-
-                }
-            }, INTERVAL)
+            setInterval(placeImg.bind(null, idx, $row), INTERVAL)
         }, idx * (INTERVAL / $rows.length))
     });
-
 
 })(this, jQuery);

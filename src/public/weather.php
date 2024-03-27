22354cb82a57fe6edd5a86b7e1de715b8b04d48e
Stephen <stephen@behnfeldt.pro>
2024-03-27 09:05:18 -0400

    Get current weather conditions from live API server, cache (for 4 hours, for development)

--- src/public/weather.php
+++ src/public/weather.php
@@ -1,6 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
+$config = json_decode(file_get_contents('../config.json'), true);
 if ( ! file_exists('../sessions')) {
     if ( ! mkdir('../sessions')) {
         die('Cannot make sessions directory');
@@ -11,19 +12,25 @@ if ( ! session_start()) {
     die('Cannot start session');
 }
 
+$now     = time();
+$current = null;
+if (file_exists('../data/current-weather.json')) {
+    $t   = filemtime('../data/current-weather.json');
+    $age = $now - $t;
+    if ($age < (4 * 60 * 60)) {
+        // cache for 4 hours (for testing)
+        $current = file_get_contents('../data/current-weather.json');
+    }
+}
+if ( ! $current) {
+    $config      = $config['weather'];
+    $queryString = http_build_query($config);
+    $apiUrl      = sprintf('https://api.openweathermap.org/data/2.5/weather?%s', $queryString);
+    $current     = file_get_contents($apiUrl);
+    $f           = file_put_contents('../data/current-weather.json', $current);
+}
 
-$config      = json_decode(file_get_contents('../config.json'), true);
-$config      = $config['weather'];
-$queryString = http_build_query($config);
-$apiUrl      = sprintf('https://api.openweathermap.org/data/2.5/weather?%s', $queryString);
-
-$current = json_decode(file_get_contents('current-weather.json'), true);
-//$current = file_get_contents( 'https://google.com');
-//$current = file_get_contents( $apiUrl);
 header('Content-Type: application/json');
-echo(json_encode($current));
-
-
+echo($current);
 
 //$forecast = file_get_contents( sprintf( 'https://api.openweathermap.org/data/2.5/forecast?%s', $queryString));
-

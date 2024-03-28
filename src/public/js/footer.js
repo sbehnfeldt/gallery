4788b491eb65af7f33d2a572a61c5134e6fafab0
Stephen <stephen@behnfeldt.pro>
2024-03-28 13:40:57 -0400

    Arrange the fetching and building current conditions

--- src/public/js/footer.js
+++ src/public/js/footer.js
@@ -1,21 +1,38 @@
 (async function (global, $) {
     'use strict';
 
-    async function getCurrent() {
-        const response = await fetch('/weather.php?weather');
-        return await response.json();
-    }
-
-    async function getForecast() {
-        const response = await fetch('/weather.php?forecast');
-        return await response.json();
-    }
-
-    let now     = Date.now();
     let $footer = $('footer').first();
 
-    let current  = await getCurrent();
-    let forecast = await getForecast();
-    $footer.html(`<img src="https://openweathermap.org/img/wn/${current['weather'][0]['icon']}@2x.png" /><span> Current Conditions: <span class="conditions">${current['weather'][0]['description']}</span> ${Math.round(current['main']['temp'])} &deg;</span>`);
+    async function doCurrentConditions() {
+        let response = await fetch('/weather.php?weather');
+        let current  = await response.json();
 
+        let $current = $footer.find('span.current');
+        let $imgs    = $current.find('span.images');
+        $imgs.empty();
+        let $text = $current.find('span.text')
+        $text.empty();
+
+        let descriptions = [];
+        for (let i = 0; i < current['weather'].length; i++) {
+            let weather = current['weather'][i];
+
+            let $img = $(`<img src="https://openweathermap.org/img/wn/${weather['icon']}@2x.png" />`);
+            $imgs.append($img);
+
+            descriptions.push(weather['description']);
+        }
+        descriptions[0] = descriptions[0].charAt(0).toUpperCase() + descriptions[0].slice(1);
+        $text.html(`${descriptions.join(', ')}. ${Math.round(current['main']['temp'])} &deg;`);
+
+        setTimeout(doCurrentConditions, 5 * 60 * 1000);
+    }
+
+    doCurrentConditions();
+
+
+    let now = Date.now();
+    // console.log(now);
+    // console.log(moment(now));
+    // console.log( moment(now).format('YYYY-MM-D'));
 })(this, jQuery);

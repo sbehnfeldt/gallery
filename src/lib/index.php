20bb11ff1c705f8b6b1caa03d30ec3e1f6e8e447
Stephen <stephen@behnfeldt.pro>
2024-04-03 09:27:24 -0400

    Added "Falling Leaves" animation based on https://www.youtube.com/watch?v=fD_AuhsheuU

--- src/lib/index.php
+++ src/lib/index.php
@@ -4,7 +4,8 @@ global $config; ?>
 <html lang="en">
     <head>
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
-        <link rel="stylesheet" href="/main.css">
+        <!--        <link rel="stylesheet" href="/main.css">-->
+        <link rel="stylesheet" href="/style.css">
         <meta charset="UTF-8">
         <title>Camelot Gallery</title>
     </head>
@@ -22,6 +23,41 @@ global $config; ?>
                 endfor; ?>
             </section>
         </main>
+        <section class="falling-leaves">
+            <div class="set">
+                <div><img src="/imgs/leaf1.png" alt=""></div>
+                <div><img src="/imgs/leaf2.png" alt=""></div>
+                <div><img src="/imgs/leaf3.png" alt=""></div>
+                <div><img src="/imgs/leaf4.png" alt=""></div>
+                <div><img src="/imgs/leaf5.png" alt=""></div>
+                <div><img src="/imgs/leaf6.png" alt=""></div>
+                <div><img src="/imgs/leaf7.png" alt=""></div>
+                <div><img src="/imgs/leaf8.png" alt=""></div>
+                <div><img src="/imgs/leaf9.png" alt=""></div>
+            </div>
+            <div class="set set2">
+                <div><img src="/imgs/leaf9.png" alt=""></div>
+                <div><img src="/imgs/leaf8.png" alt=""></div>
+                <div><img src="/imgs/leaf6.png" alt=""></div>
+                <div><img src="/imgs/leaf7.png" alt=""></div>
+                <div><img src="/imgs/leaf5.png" alt=""></div>
+                <div><img src="/imgs/leaf1.png" alt=""></div>
+                <div><img src="/imgs/leaf2.png" alt=""></div>
+                <div><img src="/imgs/leaf3.png" alt=""></div>
+                <div><img src="/imgs/leaf4.png" alt=""></div>
+            </div>
+            <div class="set set3">
+                <div><img src="/imgs/leaf7.png" alt=""></div>
+                <div><img src="/imgs/leaf8.png" alt=""></div>
+                <div><img src="/imgs/leaf1.png" alt=""></div>
+                <div><img src="/imgs/leaf5.png" alt=""></div>
+                <div><img src="/imgs/leaf9.png" alt=""></div>
+                <div><img src="/imgs/leaf2.png" alt=""></div>
+                <div><img src="/imgs/leaf4.png" alt=""></div>
+                <div><img src="/imgs/leaf6.png" alt=""></div>
+                <div><img src="/imgs/leaf3.png" alt=""></div>
+            </div>
+        </section>
         <footer>
             <div class="weather">
                 <span class="current">Current Conditions:
